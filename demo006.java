c59f790802543c47f4e890a6da13d236e88a32a0
varshith642 <tvarshith1246@gmail.com>
2025-01-03 10:13:35 +0530

    Add files via upload

---
+++ demo006.java
@@ -0,0 +1,11 @@
+public class demo006 {
+    public static void main(String[] args) {
+
+        String name = "Varshith";
+        String password = "Varshith@123";
+        while((name == "Varshith") && (password == "Varshith@123")) {
+            System.out.println("Hello varshith");
+            break;
+        }
+    }
+    }
