08b8b5e30cba2904a98a8db63ac5d1c0ca61297b
varshith642 <tvarshith1246@gmail.com>
2025-01-03 09:54:23 +0530

    Add files via upload

---
+++ demo005.java
@@ -0,0 +1,12 @@
+public class demo005 {
+    public static void main(String[] args){
+        //verify user credentials username = user001 pwd = user_001
+        String username = "user001";
+        String password = "user_001";
+
+        while((username == "user001") && (password == "user_001")) {
+            System.out.println("hello user happy new year");
+            break;
+        }
+    }
+}
