32717f9b4519ef97d98e4eda9e14d8745360dbb9
varshith642 <tvarshith1246@gmail.com>
2025-02-05 20:46:13 +0530

    Add files via upload

---
+++ Demo062.java
@@ -0,0 +1,12 @@
+public class Demo062 {
+    public static void main(String[] args) {
+        // Create an array with some elements
+        int[] array = {10, 20, 30, 40, 50};
+        // Access the 1st element (array[0])
+        System.out.println("1st Element: " + array[0]);
+        // Access the 2nd element (array[1])
+        System.out.println("2nd Element: " + array[1]);
+        // Access the last element using array.length - 1
+        System.out.println("Last Element: " + array[array.length - 1]);
+    }
+}
